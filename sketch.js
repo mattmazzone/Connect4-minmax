3f8c7af01a3b4fa6df3144e2c62f63148cfe5a64
Matteo <imatteomazzone@gmail.com>
2021-12-02 01:49:22 -0500

    Improvements
    Converted to integer array
    Changed Rendering
    CheckWin function is now boolean
    Now column selection is implemented instead of cell selection

--- sketch.js
+++ sketch.js
@@ -1,21 +1,22 @@
 function make2DArray(cols, rows) {
-    var arr = new Array(cols);
-    for (var i = 0; i < arr.length; i++) {
-        arr[i] = new Array(rows);
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows).fill(0);
     }
     return arr;
 }
 
-var grid;
+let grid;
 
-var cols;
-var rows;
+let cols;
+let rows;
 
-var d;
+let d;
+let xPos;
+let yPos;
 
-var playerTurn = true;
-
-var gameOver = false;
+let pcTurn = false;
+let gameOver = false;
 
 
 function setup() {
@@ -28,159 +29,164 @@ function setup() {
     d = 80;
 
     grid = make2DArray(cols, rows);
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            grid[i][j] = new c4piece(i, j, d);
-        }
-    }
+
 }
-
-
 
 
 function draw() {
 
+    if (pcTurn){
+        randomComputer();
+    }
+
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
-            grid[i][j].show();
+
+            xPos = (i * d * 1.2) + d;
+            yPos = (j * d * 1.2) + d;
+
+            if (grid[i][j] === 0) {
+                fill(255);
+            } else if (grid[i][j] === 1) {
+                fill(0);
+            } else if (grid[i][j] === 2) {
+                fill(255, 0, 0);
+            }
+            noStroke();
+            circle(xPos, yPos, d);
         }
     }
 
-    
 
 }
 
 
 function mousePressed() {
 
-    if(!gameOver){
-
-    if (playerTurn){
+    if (!gameOver && !pcTurn) {
 
         for (let i = 0; i < cols; i++) {
             for (let j = 0; j < rows; j++) {
-                if (dist(mouseX, mouseY, i * d*1.2+d, j * d*1.2+d) < d/2){
-                    
+                if (dist(mouseX, 0, i * d * 1.2 + d, 0) < d / 2) {
 
-                    //Checks if clicked cell is the lowest in the board
-                    
 
-                    //Places a red token if cell is empty and lowest in board
-                    if (lowestEmpty(i) === j && !grid[i][j].red && !grid[i][j].black){
-                        grid[i][j].red = true;
-                        grid[i][j].empty = false;
-                        playerTurn = false;
-                    break;
+                    // Places a red token if cell is empty and lowest in board
+                    if (lowestEmpty(i) != -1) {
+                        grid[i][lowestEmpty(i)] = 2;
+
+                        break;
                     }
+                    // If column is full computer should not play
+                    return;
 
-                    
                 }
             }
         }
+
+        if (checkWin()) {
+            alert('The game is over')
+
+        }
+        pcTurn = true;
+    } else {
+        alert('The game is over')
     }
-    checkWin();
-    randomComputer(); 
-    checkWin();
-}
-else{
-    alert('The game is over')
-}
 
 }
 
 
 function lowestEmpty(i) {
-    var lowest = -1;
+    let lowest = -1;
 
-    for (let j = 0; j<rows; j++){
-        
-        if (grid[i][j].empty){
-            lowest = j;       
+    for (let j = 0; j < rows; j++) {
+
+        if (grid[i][j] === 0) {
+            lowest = j;
         }
     }
-    console.log(lowest)
+
     return lowest;
 }
 
 
-function randomComputer(){
-    if (!playerTurn){
+function randomComputer() {
+    if (!gameOver) {
 
         let computerY = -1;
-        let computerx = -1;
-                
-        while (computerY === -1){
+        let computerX = -1;
+
+        while (computerY === -1) {
             computerX = Math.floor(Math.random() * cols);
             computerY = lowestEmpty(computerX, 0);
         }
-        grid[computerX][computerY].black = true;
-        grid[computerX][computerY].empty = false;
-        playerTurn = true;
-        
-
+        grid[computerX][computerY] = 1;
     }
+    pcTurn = false;
+    checkWin();
 }
 
-function checkWin(){
+function checkWin() {
 
-    //Vertical connect4
+    // Vertical connect4
     for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows -3; j++) {
-             
+        for (let j = 0; j < rows - 3; j++) {
 
-            if(grid[i][j].red && grid[i][j+1].red && grid[i][j+2].red && grid[i][j+3].red) {
+
+            if (grid[i][j] === 2 && grid[i][j + 1] === 2 && grid[i][j + 2] === 2 && grid[i][j + 3] === 2) {
                 console.log('red wins');
                 gameOver = true;
-            }
-            if(grid[i][j].black && grid[i][j+1].black && grid[i][j+2].black && grid[i][j+3].black) {
+                return true
+            } else if (grid[i][j] === 1 && grid[i][j + 1] === 1 && grid[i][j + 2] === 1 && grid[i][j + 3] === 1) {
                 console.log('black wins');
                 gameOver = true;
+                return true
             }
         }
     }
 
-    //Horizontal connect4
-    for (let i = 0; i < cols-3; i++) {
+    // Horizontal connect4
+    for (let i = 0; i < cols - 3; i++) {
         for (let j = 0; j < rows; j++) {
-             
 
-            if(grid[i][j].red && grid[i+1][j].red && grid[i+2][j].red && grid[i+3][j].red) {
+
+            if (grid[i][j] === 2 && grid[i + 1][j] === 2 && grid[i + 2][j] === 2 && grid[i + 3][j] === 2) {
                 console.log('red wins');
                 gameOver = true;
-            }
-            else if(grid[i][j].black && grid[i+1][j].black && grid[i+2][j].black && grid[i+3][j].black) {
+                return true;
+            } else if (grid[i][j] === 1 && grid[i + 1][j] === 1 && grid[i + 2][j] === 1 && grid[i + 3][j] === 1) {
                 console.log('black wins');
                 gameOver = true;
+                return true;
             }
         }
     }
 
-    //Diagonal connect4 (1)
-    for (let i = 0; i < cols-3; i++) {
-        for (let j = 0; j < rows-3; j++) {
-             
+    // Diagonal connect4 (1)
+    for (let i = 0; i < cols - 3; i++) {
+        for (let j = 0; j < rows - 3; j++) {
 
-            if(grid[i][j].red && grid[i+1][j+1].red && grid[i+2][j+2].red && grid[i+3][j+3].red) {
+
+            if (grid[i][j] === 2 && grid[i + 1][j + 1] === 2 && grid[i + 2][j + 2] === 2 && grid[i + 3][j + 3] === 2) {
                 console.log('red wins');
                 gameOver = true;
-            }
-            else if(grid[i][j].black && grid[i+1][j+1].black && grid[i+2][j+2].black && grid[i+3][j+3].black) {
+                return true;
+            } else if (grid[i][j] === 1 && grid[i + 1][j + 1] === 1 && grid[i + 2][j + 2] === 1 && grid[i + 3][j + 3] === 1) {
                 console.log('black wins');
                 gameOver = true;
-            }
-
-            else if(grid[i][j+3].red && grid[i+1][j+2].red && grid[i+2][j+1].red && grid[i+3][j].red) {
+                return true;
+            } else if (grid[i][j + 3] === 2 && grid[i + 1][j + 2] === 2 && grid[i + 2][j + 1] === 2 && grid[i + 3][j] === 2) {
                 console.log('red wins');
                 gameOver = true;
-            }
-            else if(grid[i][j+3].black && grid[i+1][j+2].black && grid[i+2][j+1].black && grid[i+3][j].black) {
+                return true;
+            } else if (grid[i][j + 3] === 1 && grid[i + 1][j + 2] === 1 && grid[i + 2][j + 1] === 1 && grid[i + 3][j] === 1) {
                 console.log('black wins');
                 gameOver = true;
+                return true;
             }
 
         }
     }
 
-
+    return false;
 
 }
